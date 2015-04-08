Josh/Jack - Updated data_analysis to provide the RMS and percentage of correct signs. Also refactored to clean it up a bit and pulled out penalty and C value from LR to command line arguments.

data_analysis.py
@@ -11,56 +11,87 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_extraction import DictVectorizer
 import pdb
+import math
+from numpy.core.umath import sign
+#Seed the random so that we can compare code changes
+random.seed(12345)
+
+def prepareTrainingData(train_data, questions_data):
+    # Parse the tokenized text as a dictionary and set the values as the keys (bag-of-words)
+    questions_data.tokenized_text = questions_data.tokenized_text.map(lambda x:{key:value for (key, value) in (ast.literal_eval(x)).iteritems()})
+    # Get the categories we're working with:
+    categories = pd.Series(questions_data[["category"]].values.ravel()).unique()
+    # Replace the category name with it's corresponding index
+    questions_data.category = questions_data.category.map(lambda x:np.where(categories == x)[0][0])
+    # Get the text length for all the questions
+    questions_data["text_length"] = questions_data.tokenized_text.map(lambda x:len(x))
+    
+    # Build the training set
+    train = pd.merge(right=questions_data, left=train_data, left_on="question", right_index=True)
+    train_X = train[['user', 'text_length', 'category', 'question']]
+    # dicts = list(x[0] for x in train_X[['tokenized_text']].values)
+    # v = DictVectorizer()
+    # v.fit_transform(dicts)
+    train_y = train[['position']]
+    
+    return train_X, train_y
+
+def calcRMS(prediction, actual):
+    x = 0.0
+    for i in range(0,len(actual)):
+        x += (prediction[i] - actual[i])**2
+    x /= (len(actual))
+    return math.sqrt(x)
+
+def signMismatchPercentage(prediction,actual):
+    x = 0.0
+    for i in range(0,len(actual)):
+        x += (sign(prediction[i]) == sign(actual[i]))    
+    return x*100/len(actual)
+
+def doAnalysis(prediction, actual):
+    print "-------Analysis----------"
+    rms = calcRMS(prediction,actual)
+    print ("Obtained an RMS of: %.2f" % rms)
+    signAccuracy = signMismatchPercentage(prediction,actual)
+    print ("Predicted Correct Sign %.2f%% of the time." % signAccuracy)
+    print "-------End Analysis------"
+
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--subsample", help="Percentage of the dataset to use", type=float, default=1.0, required=False)
+    argparser.add_argument("--penalty", help="Penalty Function for L.R. (l1 or l2)", type=str, default="l1", required=False)
+    argparser.add_argument("--cVal", help="C value for L.R.", type=float, default=10, required=False)
     args = argparser.parse_args()
 
     train_data = pd.read_csv('Data/train.csv', index_col=0)
     test_data = pd.read_csv('Data/test.csv', index_col=0)
     questions_data = pd.read_csv('Data/questions.csv', index_col=0)
 
-    # Parse the tokenized text as a dictionary and set the values as the keys (bag-of-words)
-    questions_data.tokenized_text = questions_data.tokenized_text.map(lambda x: {key:value for key,value in (ast.literal_eval(x)).iteritems()})
-
-    # Get the categories we're working with:
-    categories = pd.Series(questions_data[["category"]].values.ravel()).unique()
-
-    # Replace the category name with it's corresponding index
-    questions_data.category = questions_data.category.map(lambda x: np.where(categories == x)[0][0])
-
-    # Get the text length for all the questions
-    questions_data["text_length"] = questions_data.tokenized_text.map(lambda x: len(x))
-
-    # Build the training set
-    train = pd.merge(right=questions_data, left=train_data, left_on="question", right_index=True)
-
-    train_X = train[['user', 'text_length', 'category', 'question']]
-    # dicts = list(x[0] for x in train_X[['tokenized_text']].values)
-    # v = DictVectorizer()
-    # v.fit_transform(dicts)
-    train_y = train[['position']]
-
-    # Regularize the training set
-    #train_X = StandardScaler().fit_transform(train_X)
+    train_X, train_y = prepareTrainingData(train_data, questions_data)
 
     # Split the training set into dev_test and dev_train
-    X_train, X_test, y_train, y_test = train_test_split(train_X, train_y, train_size=args.subsample*0.75, test_size=args.subsample*0.25, random_state=int(random.random()*100))
+    x_train, x_test, y_train, y_test = train_test_split(train_X, train_y, train_size=args.subsample*0.75, test_size=args.subsample*0.25, random_state=int(random.random()*100))
 
     # Train LogisticRegression Classifier
-    C = 10
-    logReg = LogisticRegression(C=C, penalty='l1', tol=0.01)
-    logReg.fit_transform(X_train, y_train.ravel())
+    
+    logReg = LogisticRegression(C=args.cVal, penalty=args.penalty, tol=0.01,random_state=random.randint(0,1000000))
+    logReg.fit_transform(x_train, y_train.ravel())
     coef = logReg.coef_.ravel()
     sparsity = np.mean(coef == 0) * 100
-    print("C=%.2f" % C)
-    print("Sparsity with L1 penalty: %.2f%%" % sparsity)
-    print("score with L1penalty: %.4f" % logReg.score(X_test, y_test.ravel()))
-
+    print("C=%.2f" % args.cVal)
+    print("Sparsity with %s penalty: %.2f%%" % (args.penalty,sparsity))
+    print("score with %s penalty: %.4f" % (args.penalty,logReg.score(x_test, y_test.ravel())))
+    
+    y_test_predict = logReg.predict(x_test)
+    
+    doAnalysis(y_test_predict,y_test.ravel())
+    
+    
+    """ Now re-fit the Model on the full data set """
     # re-train on dev_test + dev_train
-    logReg.fit_transform(train_X, train_y)
-
+    logReg.fit_transform(train_X, train_y.as_matrix(["position"]).ravel())
 
     # Build the test set
     test = pd.merge(right=questions_data, left=test_data, left_on="question", right_index=True)
@@ -69,5 +100,5 @@ if __name__ == "__main__":
     # Get predictions
     predictions = logReg.predict(test)
     test_data["position"] = predictions
-    test_data.to_csv("guess.csv")
+    test_data.to_csv("Data/guess.csv")
 