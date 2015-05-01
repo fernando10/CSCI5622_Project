ba47f1a383c8c0216aa0a35bc9dda442c8d24ecb
joshkillinger <josh.killinger@gmail.com>
2015-05-01 17:02:56 -0600

    Updated final prediction to utilize 2-stage prediction

--- data_analysis.py
+++ data_analysis.py
@@ -19,9 +19,7 @@ from sklearn.svm.classes import SVC
 #Seed the random so that we can compare code changes
 random.seed(12345)
 
-def prepareTrainingData(train_data, questions_data):
-    print "Preparing Training Data"
-    # Parse the tokenized text as a dictionary and set the values as the keys (bag-of-words)
+def prepareQuestionData(questions_data):
     questions_data.tokenized_text = questions_data.tokenized_text.map(lambda x:{key:value for (key, value) in (ast.literal_eval(x)).iteritems()})
     # Get the categories we're working with:
     categories = pd.Series(questions_data[["category"]].values.ravel()).unique()
@@ -32,15 +30,44 @@ def prepareTrainingData(train_data, questions_data):
     
     questions_data.ner = questions_data.ner.map(buildWordCategoryFeatures)
 
+    return questions_data, categories
+
+def prepareTrainingData(train_data, questions_data):
+    print "Preparing Training Data"
     # Build the training set
+    
     train = pd.merge(right=questions_data, left=train_data, left_on="question", right_index=True)
     train_X = train[['user', 'text_length', 'category', 'question','ner']]
-    # dicts = list(x[0] for x in train_X[['tokenized_text']].values)
-    # v = DictVectorizer()
-    # v.fit_transform(dicts)
     train_y = train[['position']]
     
-    return train_X, train_y, categories
+    train_X = train_X.as_matrix()
+    x_len = len(train_X[0])
+    temp = np.zeros((len(train_X),x_len + len(train_X[0][-1]) -1))
+    for i in range(len(train_X)):
+        b = list(train_X[i][0:x_len - 1])
+        b.extend(train_X[i][-1])
+        temp[i] = np.array(b)
+    train_X = temp
+    
+    return train_X, train_y
+
+def prepareTestData(test_data, questions_data):
+    print "Preparing Test Data"
+    # Build the test set
+    
+    test = pd.merge(right=questions_data, left=test_data, left_on="question", right_index=True)
+    test_X = test[['user', 'text_length', 'category', 'question','ner']]
+    
+    test_X = test_X.as_matrix()
+    x_len = len(test_X[0])
+    temp = np.zeros((len(test_X),x_len + len(test_X[0][-1]) -1))
+    for i in range(len(test_X)):
+        b = list(test_X[i][0:x_len - 1])
+        b.extend(test_X[i][-1])
+        temp[i] = np.array(b)
+    test_X = temp
+
+    return test_X
 
 digitRegex = re.compile("\d+")
 def buildWordCategoryFeatures(nerString):
@@ -62,6 +89,12 @@ def buildWordCategoryFeatures(nerString):
     vectorizer = DictVectorizer()
     return vectorizer.fit_transform(positionValues).toarray()[0].tolist()
 
+def appendBuzzPosition(x_data, positions):
+    x_data_2 = np.zeros((len(x_data),len(x_data[0]) + 1))
+    for i in range(len(x_data_2)):
+        x_data_2[i] = np.append(x_data[i], abs(positions[i]))
+
+    return x_data_2
 
 def calcRMS(prediction, actual):
     if (len(actual) == 0):
@@ -111,7 +144,7 @@ if __name__ == "__main__":
     argparser.add_argument("--penalty", help="Penalty Function for L.R. (l1 or l2)", type=str, default="l2", required=False)
     argparser.add_argument("--cVal", help="C value for L.R.", type=float, default=10, required=False)
     argparser.add_argument("--twoStage", help="Splits the classification into two stages, position and correctness", type=bool, default=True, required=False)
-    argparser.add_argument("--truncateInput", help="Number of lines to truncate the input files to when parsing", type=int, default=100, required=False)
+    argparser.add_argument("--truncateInput", help="Number of lines to truncate the input files to when parsing", type=int, default=0, required=False)
     args = argparser.parse_args()
 
     train_data = pd.read_csv('Data/train.csv', index_col=0)
@@ -123,15 +156,9 @@ if __name__ == "__main__":
         test_data = test_data[:args.truncateInput]
         questions_data = questions_data[:args.truncateInput]
 
-    train_X, train_y,categories = prepareTrainingData(train_data, questions_data)
-    train_X = train_X.as_matrix()
-    x_len = len(train_X[0])
-    temp = np.zeros((len(train_X),x_len + len(train_X[0][-1]) -1))
-    for i in range(len(train_X)):
-        b = list(train_X[i][0:x_len - 1])
-        b.extend(train_X[i][-1])
-        temp[i] = np.array(b)
-    train_X = temp
+    questions_data, categories = prepareQuestionData(questions_data)
+    train_X, train_y = prepareTrainingData(train_data, questions_data)
+    
     # Split the training set into dev_test and dev_train
     x_train, x_test, y_train, y_test = train_test_split(train_X, train_y, train_size=args.subsample*0.75, test_size=args.subsample*0.25, random_state=int(random.random()*100))
     
@@ -156,17 +183,13 @@ if __name__ == "__main__":
      
     y_test_predict = logReg.predict(x_test)
     
-    #add the buzz position as a feature for the correctness classifier
-    y_test_predict_r2 = []
-    x_train_r2 = np.zeros((len(x_train),len(x_train[0]) + 1))
-    x_test_r2 = np.zeros((len(x_test),len(x_train[0]) + 1))
-
-    logregCorrect = None
+    svmCorrect = None
     if args.twoStage:
+        #add the buzz position as a feature for the correctness classifier
+        x_train_r2 = appendBuzzPosition(x_train, y_train)
+        x_test_r2 = appendBuzzPosition(x_test, y_test_predict)
+    
         print "Performing second stage fit"
-        for i in range(len(x_train)):
-            x_train_r2[i] = np.append(x_train[i], abs(y_train[i]))
-        
         #logregCorrect = LogisticRegression(C=1, penalty=args.penalty, tol=0.01)#, random_state=random.randint(0,1000000))
         #logregCorrect.fit_transform(x_train_r2, sign(y_train))
         
@@ -174,9 +197,6 @@ if __name__ == "__main__":
         svmCorrect = SVC(C=1, class_weight=class_Weights)
         svmCorrect.fit(x_train_r2, sign(y_train))
         
-        for i in range(len(x_test)):
-            x_test_r2[i] = np.append(x_test[i], y_test_predict[i])
-            
         print "Performing second stage prediction"
         #y_test_predict_r2 = logregCorrect.predict(x_test_r2)
         y_test_predict_r2 = svmCorrect.predict(x_test_r2)
@@ -195,16 +215,25 @@ if __name__ == "__main__":
     doAnalysis(y_test_predict,y_test,x_test,categories)
     
     
-    """ Now re-fit the Model on the full data set 
+    '''Now re-fit the Model on the full data set''' 
     # re-train on dev_test + dev_train
-    logReg.fit_transform(train_X, train_y.as_matrix(["position"]).ravel())
+    train_y = train_y.as_matrix().ravel()
+    logReg.fit_transform(train_X, abs(train_y) if args.twoStage else train_y)
 
     # Build the test set
-    test = pd.merge(right=questions_data, left=test_data, left_on="question", right_index=True)
-    test = test[['user', 'text_length', 'category', 'question']]
+    test = prepareTestData(test_data, questions_data)
 
     # Get predictions
     predictions = logReg.predict(test)
+    
+    if (args.twoStage):
+        train_X = appendBuzzPosition(train_X, train_y)
+        test = appendBuzzPosition(test, predictions)
+        
+        svmCorrect.fit(train_X, sign(train_y))
+        predictions_2 = svmCorrect.predict(test)
+        for i in range(len(predictions_2)):
+            predictions[i] *= sign(predictions_2[i])
+    
     test_data["position"] = predictions
     test_data.to_csv("Data/guess.csv")
-    """
